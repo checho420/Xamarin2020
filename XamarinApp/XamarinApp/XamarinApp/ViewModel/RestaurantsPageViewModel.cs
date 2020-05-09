36c44287db094abd65171805f5c418c15a72d9a3
checho420 <chechosk8@gmail.com>
2020-05-09 12:53:55 -0500

    Tabs, restaurantes y servicios

---
+++ XamarinApp/XamarinApp/XamarinApp/ViewModel/RestaurantsPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using XamarinApp.Models;
+using XamarinApp.Repositories;
+
+namespace XamarinApp.ViewModel
+{
+    public class RestaurantsPageViewModel
+    {
+        public ObservableCollection<RestaurantModel> Restaurantes { get; set; }
+
+
+
+        public RestaurantsPageViewModel()
+        {
+            Restaurantes = new ObservableCollection<RestaurantModel>();
+            LoadRestaurants();            
+
+            /*
+           Restaurantes = new List<RestaurantModel>();           
+
+           Restaurantes.Add(new RestaurantModel
+           {
+               Id = Guid.NewGuid(),
+               Nombre = "Kaskitos",
+               Direccion = "Caldas",
+               Telefono = "3214569874",
+               Imagen = "https://media-cdn.tripadvisor.com/media/photo-s/15/47/48/a9/la-letra-b-insignia-de.jpg",
+               SitioWeb = "www.pillalo.co"
+           });
+           Restaurantes.Add(new RestaurantModel
+           {
+               Id = Guid.NewGuid(),
+               Nombre = "Empanadas doña Martha",
+               Direccion = "Caldas",
+               Telefono = "3214569874",
+               Imagen = "https://media-cdn.tripadvisor.com/media/photo-s/17/a0/bb/85/t-bone-steak.jpg",
+               SitioWeb = "www.pillalo.co"
+           });
+           Restaurantes.Add(new RestaurantModel
+           {
+               Id = Guid.NewGuid(),
+               Nombre = "Kaskitos",
+               Direccion = "Caldas",
+               Telefono = "3214569874",
+               Imagen = "https://media-cdn.tripadvisor.com/media/photo-s/15/47/48/a9/la-letra-b-insignia-de.jpg",
+               SitioWeb = "www.pillalo.co"
+           });
+           Restaurantes.Add(new RestaurantModel
+           {
+               Id = Guid.NewGuid(),
+               Nombre = "Empanadas doña Martha",
+               Direccion = "Caldas",
+               Telefono = "3214569874",
+               Imagen = "https://media-cdn.tripadvisor.com/media/photo-s/17/a0/bb/85/t-bone-steak.jpg",
+               SitioWeb = "www.pillalo.co"
+           });
+           */
+
+
+        }
+        async private void LoadRestaurants()
+        {
+            foreach (var item in await new RestaurantRepository().GetRestaurants())
+
+            {
+                Restaurantes.Add(item);
+
+            }
+        }
+
+
+    }
+}
--- XamarinApp/XamarinApp/XamarinApp/Views/LoginPage2.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/Views/LoginPage2.xaml.cs
@@ -17,6 +17,31 @@ namespace XamarinApp.Views
             InitializeComponent();
         }
 
-        void Login(object sender, EventArgs e) { }
+        async private void btn_login_Clicked(object sender, EventArgs e)
+        {
+
+#if DEBUG
+
+            txt_email.Text = "checho420";
+            txt_pass.Text = "admin";
+
+#endif
+
+            string email = txt_email.Text;
+            string pass = txt_pass.Text;
+
+            if(email =="checho420" && pass == "admin")
+            {
+
+                await this.Navigation.PushModalAsync(new HomePage());
+                //this.DisplayAlert("Exito", "usuario o pass correcto", "Aceptar");
+            }
+            else
+            {
+                await this.DisplayAlert("Error", "usuario o pass icorrectos", "Aceptar");
+            }
+           
+
+        }
     }
 }
